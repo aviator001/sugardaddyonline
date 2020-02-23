04829c78ab4c5f71a964d3d4848a85fb7fd357d3
Gautam Sharma <g@terrawire.com>
2020-02-23 15:42:23 +0530

    e

--- track.js
+++ track.js
@@ -1,13 +1,7 @@
 setTimeout(function(){
-	var xp=window.location.href.toUpperCase()
-	var arr=['HOME','SETTINGS','PROFILEBROWSE','INBOX','SENDMESSAGE','MEMBER_WISHLIST','SEARCH','ALBUMS','FRAME_WISH','CHECKOUT','LOGOUT']
-	var arrDesc=['HOME','SETTINGS','PROFILEBROWSE','INBOX','SENDMESSAGE','MEMBER_WISHLIST','SEARCH','ALBUMS','FRAME_WISH','CHECKOUT','LOGOUT']
-	var i
-	for (var i=0;i<arr.length-1;i++) {
-		if (xp.indexOf(arr[i])>=0) action=arrDesc[i]
-	}
-	var url='https://sugardaddylocal.com/x_track_user.php?mid='+getCookie('mid')+'&action='+action.toLowerCase()
+	var action=window.location.href.split('.html')[0].split('/')[window.location.href.split('.html')[0].split('/').length-1].toUpperCase()
+	var url='https://sugardaddylocal.com/x_track_user.php?mid='+getCookie('mid')+'&action='+action
 	console.log(url)
 	$.ajax({url:url})
-},100)
+},3000)
 			
